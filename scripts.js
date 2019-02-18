314b15814786bfd049a8fcf3f83f937f90011ed4
Greg Davis <gdavis@davtechsolutions.com>
2019-02-18 03:00:46 -0500

    Updated portfolio hover effects. Updated Utilities project description.

--- scripts.js
+++ scripts.js
@@ -17,13 +17,15 @@ const frames = document.getElementsByClassName('frame');
 
 for (let i = 0; i < frames.length; i += 1) {
     frames[i].addEventListener('mouseenter', () => {
-        frames[i].style.borderRadius = '25px';
+        //frames[i].style.borderRadius = '25px';
+        frames[i].style.transform = "scale(1.1)";
         frames[i].style.backgroundColor = '#d9d9d9'; //Slightly darker gray focus.
     });
 
 
     frames[i].addEventListener('mouseleave', () => {
-        frames[i].style.borderRadius = '0';
+        //frames[i].style.borderRadius = '0';
+        frames[i].style.transform = "scale(1)";
         frames[i].style.backgroundColor = '#f2f2f2'; //Default gray
     });
 }
